f3c6de5ddfd934034202c3469a26e204a0c2f9b0
Salvador Ariza <sariza@thoughtworks.com>
2017-09-27 11:44:57 -0500

    Added extra rules to eslint to ensure good practices in code

--- app/containers/main/MainContainer.js
+++ app/containers/main/MainContainer.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default class MainContainer extends React.Component {
   render () {
-    return (<p> Hello World! </p>)
+    return (<p>{'Hello World!'}</p>)
   }
 }
