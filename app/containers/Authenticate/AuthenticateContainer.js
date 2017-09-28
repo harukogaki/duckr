c8c9abe2f52b61e5bf07b98f5bd127e777649411
Salvador Ariza <sariza@thoughtworks.com>
2017-09-28 16:09:45 -0500

    Added an authentication mock; changed AuthenticateContainer from a stateless function to a component

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Authenticate } from 'components'
+import auth from 'helpers/auth'
 
-export default function AuthenticateContainer(props){
+export default class AuthenticateContainer extends React.Component{
+
+  render(){
     return (
       <div>
         <Authenticate />
       </div>
     )
+  }
 }
