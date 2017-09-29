1f2ea2cab93c6aca012c14016ef52c48487bcef6
Salvador Ariza <sariza@thoughtworks.com>
2017-09-29 15:56:10 -0500

    bound the action creators for users to dispatch using bindActionCreators

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,48 +1,52 @@
 import React from 'react'
-import { Authenticate } from 'components'
+import {Authenticate} from 'components'
 import auth from 'helpers/auth'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
 import * as userActionCreaters from 'redux/modules/users'
 
 class AuthenticateContainer extends React.Component
 {
 
   handleAuth = () => {
-    this.props.dispatch(userActionCreaters.fetchingUser())
+    this.props.fetchingUser()
 
-    auth()
-    .then((user)=>{
-      this.props.dispatch(userActionCreaters.fetchingUserSuccess(user.uid, user, Date.now()))
+    auth().then((user) => {
+      this.props.fetchingUserSuccess(user.uid, user, Date.now())
       console.log(user)
+    }).catch((error) => {
+      this.props.userActionCreaters.fetchingUserFailure(error)
     })
-    .catch((error) => { this.props.dispatch(userActionCreaters.fetchingUserFailure(error))})
   }
 
-  render(){
+  render() {
     console.log('isFetching: ' + this.props.isFetching)
     return (
       <div>
-        <Authenticate
-          isFetching={this.props.isFetching}
-          error={this.props.error}
-          onAuth={this.handleAuth}/>
+        <Authenticate isFetching={this.props.isFetching} error={this.props.error} onAuth={this.handleAuth}/>
       </div>
     )
-  }
-}
-
-function mapStateToProps(state){
-  console.log(state);
-  return {
-    isFetching: state.isFetching,
-    error: state.error,
   }
 }
 
 AuthenticateContainer.propTypes = {
   isFetching: PropTypes.bool.isRequired,
   error: PropTypes.string.isRequired,
+  authUser: PropTypes.func.isRequired,
+  fetchingUser: PropTypes.func.isRequired,
+  fetchingUserSuccess:PropTypes.func.isRequired,
+  fetchingUserFailure:PropTypes.func.isRequired,
+
 }
 
-export default connect(mapStateToProps)(AuthenticateContainer)
+function mapStateToProps(state) {
+  console.log(state);
+  return {isFetching: state.isFetching, error: state.error}
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(userActionCreaters, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthenticateContainer)
